3dc08baa423f8a639f5d0a1900cac2d21d15f49b
knowave <kwaktaemin98@gmail.com>
2024-08-13 11:23:22 +0900

    generate board entity

--- board-spring/src/main/java/board/board_spring/domain/board/entity/Board.java
+++ board-spring/src/main/java/board/board_spring/domain/board/entity/Board.java
@@ -1,2 +1,15 @@
-package board.board_spring.domain.board.entity;public class Board {
+package board.board_spring.domain.board.entity;
+
+import board.board_spring.domain.BaseEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+
+@Entity()
+public class Board extends BaseEntity {
+    @Column(length = 50, nullable = false)
+    @NotEmpty(message = "제목을 입력해주세요.")
+    private Long title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
 }
