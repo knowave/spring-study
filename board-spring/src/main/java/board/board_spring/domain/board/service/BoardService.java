a454e1b7a7ed067f6eb3eee1331e1e756201e9dc
knowave <kwaktaemin98@gmail.com>
2024-08-14 19:44:00 +0900

    board service -> createBoard

--- board-spring/src/main/java/board/board_spring/domain/board/service/BoardService.java
+++ board-spring/src/main/java/board/board_spring/domain/board/service/BoardService.java
@@ -1,2 +1,16 @@
-package board.board_spring.domain.board.service;public class BoardService {
+package board.board_spring.domain.board.service;
+
+import board.board_spring.domain.board.dto.BoardDto;
+import board.board_spring.domain.board.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service()
+public class BoardService {
+    private final BoardRepository boardRepository;
+
+    public void createBoard(BoardDto boardDto) {
+        boardRepository.save(boardDto.toEntity());
+    }
 }
