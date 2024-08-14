ccc19975559bf30793b582f614d201f0b641e12e
knowave <kwaktaemin98@gmail.com>
2024-08-14 19:43:14 +0900

    board dto 수정

--- board-spring/src/main/java/board/board_spring/domain/board/dto/BoardDto.java
+++ board-spring/src/main/java/board/board_spring/domain/board/dto/BoardDto.java
@@ -1,5 +1,11 @@
 package board.board_spring.domain.board.dto;
 
+import board.board_spring.domain.board.entity.Board;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class BoardDto {
     private String title;
     private String content;
@@ -18,5 +24,14 @@ public class BoardDto {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public Board toEntity() {
+        Board board = Board.builder()
+                .title(title)
+                .content(content)
+                .build();
+
+        return board;
     }
 }
