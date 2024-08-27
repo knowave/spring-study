f37f0411223d9278c62e6c8d30de5de2954c489a
knowave <kwaktaemin98@gmail.com>
2024-08-27 16:48:35 +0900

    jpa 사용하기 위한 관련 어노테이션 추가 및 dotenv 변수 등록하기위한 코드 추가

--- board-spring/src/main/java/board/board_spring/BoardSpringApplication.java
+++ board-spring/src/main/java/board/board_spring/BoardSpringApplication.java
@@ -1,12 +1,19 @@
 package board.board_spring;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing()
 @SpringBootApplication
 public class BoardSpringApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(BoardSpringApplication.class, args);
 	}
 
