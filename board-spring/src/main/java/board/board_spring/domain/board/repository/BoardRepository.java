d27854d174820558b10ffed1fc25ffd75b3c4cc1
knowave <kwaktaemin98@gmail.com>
2024-08-29 16:22:34 +0900

    기존에 존재하던 메서드 제거

--- board-spring/src/main/java/board/board_spring/domain/board/repository/BoardRepository.java
+++ board-spring/src/main/java/board/board_spring/domain/board/repository/BoardRepository.java
@@ -4,5 +4,4 @@ import board.board_spring.domain.board.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
-    Board getBoardById(Long id);
 }
