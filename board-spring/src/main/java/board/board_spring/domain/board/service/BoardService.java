e8b1013a180bdec3ea0a2c4ff217dd631b3014a7
knowave <kwaktaemin98@gmail.com>
2024-08-29 16:22:14 +0900

    게시글 상세 조회

--- board-spring/src/main/java/board/board_spring/domain/board/service/BoardService.java
+++ board-spring/src/main/java/board/board_spring/domain/board/service/BoardService.java
@@ -1,8 +1,11 @@
 package board.board_spring.domain.board.service;
 
 import board.board_spring.domain.board.dto.BoardDto;
+import board.board_spring.domain.board.entity.Board;
 import board.board_spring.domain.board.repository.BoardRepository;
+import board.board_spring.global.exception.CustomException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -12,5 +15,10 @@ public class BoardService {
 
     public void createBoard(BoardDto boardDto) {
         boardRepository.save(boardDto.toEntity());
+    }
+
+    public Board getBoardById(Long id) {
+        Board board = boardRepository.findById(id).orElseThrow(() -> new CustomException("Not Found Board", HttpStatus.NOT_FOUND));
+        return board;
     }
 }
---
+++ board-spring/src/main/java/board/board_spring/global/exception/CustomException.java
@@ -0,0 +1,2 @@
+package board.board_spring.global.exception;public class CustomException {
+}
