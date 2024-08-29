def8a585236f25ca0011054ff18c7f3a2bfa3a2e
knowave <kwaktaemin98@gmail.com>
2024-08-29 16:22:53 +0900

    generate custom exception class

--- board-spring/src/main/java/board/board_spring/global/exception/CustomException.java
+++ board-spring/src/main/java/board/board_spring/global/exception/CustomException.java
@@ -1,2 +1,25 @@
-package board.board_spring.global.exception;public class CustomException {
+package board.board_spring.global.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class CustomException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    private final String message;
+    private final HttpStatus status;
+
+    public CustomException(String message, HttpStatus status) {
+        super(message);
+        this.message = message;
+        this.status = status;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
 }
