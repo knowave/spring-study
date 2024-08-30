cc309cab515964839b5dad3b7d4a10aa8c1c8818
knowave <kwaktaemin98@gmail.com>
2024-08-30 16:49:55 +0900

    게시글 상세 조회 API

--- board-spring/src/main/java/board/board_spring/domain/board/controller/BoardController.java
+++ board-spring/src/main/java/board/board_spring/domain/board/controller/BoardController.java
@@ -1,13 +1,11 @@
 package board.board_spring.domain.board.controller;
 
 import board.board_spring.domain.board.dto.BoardDto;
+import board.board_spring.domain.board.entity.Board;
 import board.board_spring.domain.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,5 +17,11 @@ public class BoardController {
     public ResponseEntity<String> createBoard(@RequestBody BoardDto boardDto) {
         boardService.createBoard(boardDto);
         return ResponseEntity.ok().body("create board success!");
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Board> getBoardById(@RequestParam Long id) {
+        Board board = boardService.getBoardById(id);
+        return ResponseEntity.ok().body(board);
     }
 }
