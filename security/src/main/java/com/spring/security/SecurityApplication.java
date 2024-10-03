9ca2a04d62f343de5ed7fa0a9688d5ba29e1bb6d
knowave <kwaktaemin98@gmail.com>
2024-10-03 18:54:21 +0900

    jpa & dotenv

--- security/src/main/java/com/spring/security/SecurityApplication.java
+++ security/src/main/java/com/spring/security/SecurityApplication.java
@@ -1,12 +1,19 @@
 package com.spring.security;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SecurityApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(SecurityApplication.class, args);
 	}
 
