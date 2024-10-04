e3eb875e94dd2929ceb5d559777c0490ac699bbf
knowave <kwaktaemin98@gmail.com>
2024-10-04 17:24:01 +0900

    User Repository 생성

--- security/src/main/java/com/spring/security/repository/UserRepository.java
+++ security/src/main/java/com/spring/security/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.spring.security.repository;
 
-public interface UserRepository {
-}
+import com.spring.security.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * JPA Repository는 두 개의 인자를 받는다.
+ * 첫번째는 Repository가 해당하는 Entity를 넣어준다.
+ * 두번째는 Entity의 id값의 타입을 레퍼런스 타입으로 넣어준다.
+ */
+public interface UserRepository extends JpaRepository<User, Integer> {}
