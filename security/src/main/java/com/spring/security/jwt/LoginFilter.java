70d210748fe94b3248520ad515f224e4f885669b
knowave <kwaktaemin98@gmail.com>
2024-10-04 19:06:53 +0900

    login filter 정의

--- security/src/main/java/com/spring/security/jwt/LoginFilter.java
+++ security/src/main/java/com/spring/security/jwt/LoginFilter.java
@@ -1,4 +1,46 @@
 package com.spring.security.jwt;
 
-public class JwtAuthenticationFilter{
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    public LoginFilter(AuthenticationManager authenticationManager) {
+
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        // 로그인 시 클라이언트 요청에서 email, password 추출
+        String email = obtainUsername(request);
+        String password = obtainPassword(request);
+
+        System.out.println(email);
+
+        // Spring Security에서 email과 password를 검증하기 위해서 token에 담아야 함.
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(email, password, null);
+
+        // Token에 담은 검증을 위한 AuthenticationManager로 전달.
+        return authenticationManager.authenticate(authToken);
+    }
+
+    // 로그인 성공 시 실행하는 메서드 (여기서 JWT Token을 발급.)
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
+
+
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {}
 }
