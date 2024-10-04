83e1889ae42e3f06c1baa07ec46d067f3ea2194e
knowave <kwaktaemin98@gmail.com>
2024-10-04 19:20:08 +0900

    User Details 커스텀 구현

--- security/src/main/java/com/spring/security/service/CustomUserDetailsService.java
+++ security/src/main/java/com/spring/security/service/CustomUserDetailsService.java
@@ -1,27 +1,28 @@
-//package com.spring.security.service;
-//
-//import com.spring.security.entity.User;
-//import com.spring.security.repository.UserRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class CustomUserDetailsService implements UserDetailsService {
-//    private final UserRepository userRepository;
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-//        User user = userRepository.findByEmail(email)
-//                .orElseThrow(() -> new UsernameNotFoundException("존재하는 사용자가 없습니다." + email));
-//
-//        return new org.springframework.security.core.userdetails.User(
-//                user.getEmail(),
-//                user.getPassword(),
-//                user.getRole().getAuthorities()
-//        );
-//    }
-//}
+package com.spring.security.service;
+
+import com.spring.security.dto.CustomUserDetails;
+import com.spring.security.entity.User;
+import com.spring.security.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("존재하는 사용자가 없습니다." + email));
+
+        return new CustomUserDetails(user);
+    }
+}
