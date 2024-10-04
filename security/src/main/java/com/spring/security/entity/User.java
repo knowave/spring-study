b1454a3fb34fc386c58e3bfdcba4d5cada4ae78c
knowave <kwaktaemin98@gmail.com>
2024-10-04 17:21:12 +0900

    User Entity 정의

--- security/src/main/java/com/spring/security/entity/User.java
+++ security/src/main/java/com/spring/security/entity/User.java
@@ -1,4 +1,31 @@
 package com.spring.security.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
+    @Id()
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // IDENTITY 설정을 해야 ID 값들이 중복으로 생성되지 않음.
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
 }
