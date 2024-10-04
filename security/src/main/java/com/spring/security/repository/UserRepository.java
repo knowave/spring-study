bdbb21ad02c62fdff5c14741f67ffd1b074a53ae
knowave <kwaktaemin98@gmail.com>
2024-10-04 18:32:01 +0900

    user 검증 메서드 추가

--- security/src/main/java/com/spring/security/repository/UserRepository.java
+++ security/src/main/java/com/spring/security/repository/UserRepository.java
@@ -8,4 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * 첫번째는 Repository가 해당하는 Entity를 넣어준다.
  * 두번째는 Entity의 id값의 타입을 레퍼런스 타입으로 넣어준다.
  */
-public interface UserRepository extends JpaRepository<User, Integer> {}
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Boolean existsByEmail(String email);
+}
