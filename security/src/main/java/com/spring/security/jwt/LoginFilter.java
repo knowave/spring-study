ac6c001774c72cd0c4cccf48f1de0fc21ebc7069
knowave <kwaktaemin98@gmail.com>
2024-10-09 17:10:55 +0900

    json body로 요청 본문에서 email과 password 추출

--- security/src/main/java/com/spring/security/jwt/LoginFilter.java
+++ security/src/main/java/com/spring/security/jwt/LoginFilter.java
@@ -1,13 +1,18 @@
 package com.spring.security.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+import java.util.Map;
 
 public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -22,11 +27,30 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
 
-        // 로그인 시 클라이언트 요청에서 email, password 추출
-        String email = request.getParameter("email");
-        String password = request.getParameter("password");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String email = null;
+        String password = null;
 
-        // Spring Security에서 email과 password를 검증하기 위해서 token에 담아야 함.
+        try {
+            // JSON 형식의 요청 본문에서 email과 password를 추출
+            Map<String, String> requestBody = objectMapper.readValue(request.getInputStream(), Map.class);
+            email = requestBody.get("email");
+            password = requestBody.get("password");
+
+            /**
+             * multipart/form-data 형식의 데이터를 처리할 땐 아래왕 같이 처리.
+             * String email = request.getParameter("email");
+             * String password = request.getParameter("password");
+             */
+
+        } catch (IOException e) {
+            throw new AuthenticationServiceException("Request parsing failed", e);
+        }
+
+        System.out.println("Email: " + email);
+        System.out.println("Password: " + password);
+
+        // Spring Security에서 email과 password를 검증하기 위해 token에 담아야 함.
         UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(email, password, null);
 
         // Token에 담은 검증을 위한 AuthenticationManager로 전달.
