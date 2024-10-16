25f7e72ed620c6eeb38de400bc416188a65ff1d2
knowave <kwaktaemin98@gmail.com>
2024-10-16 18:29:40 +0900

    refresh token 생성 메서드

--- security/src/main/java/com/spring/security/jwt/JwtProvider.java
+++ security/src/main/java/com/spring/security/jwt/JwtProvider.java
@@ -13,13 +13,15 @@ import java.util.Date;
 @Component
 public class JwtProvider {
 
-    private SecretKey secretKey;
-    private Long expirationMs;
+    private final SecretKey secretKey;
+    private final Long accessTokenExpirationMs;
+    private final Long refreshTokenExpirationMs;
 
-    private JwtProvider(@Value("${jwt.secret}") String secret, @Value("${jwt.access-token-expiration}") Long expirationMs) {
+    private JwtProvider(@Value("${jwt.secret}") String secret, @Value("${jwt.access-token-expiration}") Long accessTokenExpirationMs, @Value("${jwt.refresh-token-expiration}") Long refreshTokenExpirationMs) {
 
         this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
-        this.expirationMs = expirationMs;
+        this.accessTokenExpirationMs = accessTokenExpirationMs;
+        this.refreshTokenExpirationMs = refreshTokenExpirationMs;
     }
 
     // Email 검증
@@ -40,14 +42,25 @@ public class JwtProvider {
         return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
     }
 
-    // Token 생성
+    // Access Token 생성
     public String createJwt(String email, String role) {
 
         return Jwts.builder()
                 .claim("email", email)
                 .claim("role", role)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + expirationMs))
+                .expiration(new Date(System.currentTimeMillis() + accessTokenExpirationMs))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    // Refresh Token 생성
+    public String createRefreshToken(String email, String role) {
+        return Jwts.builder()
+                .claim("email", email)
+                .claim("role", role)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + refreshTokenExpirationMs))
                 .signWith(secretKey)
                 .compact();
     }
