de840afcd93dae49486237ed3943227781436161
knowave <kwaktaemin98@gmail.com>
2024-10-23 17:04:05 +0900

    refreshToken으로 AccessToken 발급하는 controller endPoint 허용

--- security/src/main/java/com/spring/security/config/SecurityConfig.java
+++ security/src/main/java/com/spring/security/config/SecurityConfig.java
@@ -68,6 +68,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/login", "/", "/join").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
+                        .requestMatchers("/reissue").permitAll()
                         .anyRequest().authenticated());
 
         // JWT Filter 등록
