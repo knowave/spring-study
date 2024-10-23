192b124a49c793d333e07406aaf7efa0f03830c2
knowave <kwaktaemin98@gmail.com>
2024-10-23 17:03:32 +0900

    refreshToken을 이용한 AccessToken 발급하기

---
+++ security/src/main/java/com/spring/security/controller/ReissueController.java
@@ -0,0 +1,75 @@
+package com.spring.security.controller;
+
+import com.spring.security.jwt.JwtProvider;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class ReissueController {
+
+    private final JwtProvider jwtProvider;
+
+    public ReissueController(JwtProvider jwtProvider) {
+
+        this.jwtProvider = jwtProvider;
+    }
+
+    @PostMapping("/reissue")
+    public ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response) {
+
+        // get refresh token
+        String refresh = null;
+        Cookie[] cookies = request.getCookies();
+
+        for (Cookie cookie : cookies) {
+
+            if (cookie.getName().equals("refresh")) {
+
+                refresh = cookie.getValue();
+            }
+        }
+
+        if (refresh == null) {
+
+            // response status code
+            return new ResponseEntity<>("refresh token is null", HttpStatus.BAD_REQUEST);
+        }
+
+        // expired check
+        try {
+            jwtProvider.isExpired(refresh);
+        } catch (ExpiredJwtException e) {
+
+            // response status code
+            return new ResponseEntity<>("refresh token expired", HttpStatus.BAD_REQUEST);
+        }
+
+        // 토큰이 refresh인지 확인 (발급 시 페이로드에 명시)
+        String category = jwtProvider.getCategory(refresh);
+
+        if (!category.equals("refresh")) {
+
+            // response status code
+            return new ResponseEntity<>("invalid refresh token", HttpStatus.BAD_REQUEST);
+        }
+
+        String email = jwtProvider.getEmail(refresh);
+        String role = jwtProvider.getRole(refresh);
+
+        // make new jwt
+        String newAccessToken = jwtProvider.createAccessToken(category, email, role);
+
+        // response
+        response.setHeader("access", newAccessToken);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
